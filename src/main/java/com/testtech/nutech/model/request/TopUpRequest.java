db53197c9ed5d9f6ef86289b5f2577d93efae89d
jefrisaputra <jefrisaputra7373@gmail.com>
2024-10-22 21:57:35 +0700

    IMAGE

---
+++ src/main/java/com/testtech/nutech/model/request/TopUpRequest.java
@@ -0,0 +1,31 @@
+package com.testtech.nutech.model.request;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder(toBuilder = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class TopUpRequest {
+
+    private Integer ammount;
+
+
+}
+
+
+
